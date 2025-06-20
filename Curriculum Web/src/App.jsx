cee6a07c11336f9ed9af08cd86c376c3f41e32da
andrew <andrewrfofficial@gmail.com>
2025-06-20 14:52:05 -0400

    init

---
+++ Curriculum Web/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function App() {
+  const [name,setName] = useState("Andrew Rey")
+
+  const changeName = ()=>{
+  }
+
+  return (
+    <>
+    <p className="">Hola</p>
+    </>
+  )
+}
+
+export default App
